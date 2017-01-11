0470369ab0076fe293ab860e1dff947ea173380a
m5tt <arks36@protonmail.ch>
2017-01-11 01:22:18 -0500

    initial commit

---
+++ cliupl/cliupl.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+
+
+class UplSite:
+    """
+    Abstract class for a upload site
+    """
+
+    def upload(self, img_filename):
+        raise NotImplementedError(
+            f'Class {self.__class__.__name__} doesnt implement upload()')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
